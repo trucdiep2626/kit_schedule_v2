daac41b610cdf62b255355e8ea80bf212248a17d
kit2512 <dannamdinh49@gmail.com>
2023-03-04 11:02:01 +0700

    Add dimensions

--- lib/common/constants/app_dimens.dart
+++ lib/common/constants/app_dimens.dart
@@ -13,6 +13,7 @@ class AppDimens {
   /// Space
   static double space_4 = 4.sp;
   static double space_8 = 8.sp;
+  static double space_10 = 10.sp;
   static double space_12 = 12.sp;
   static double space_14 = 14.sp;
   static double space_16 = 16.sp;
@@ -20,6 +21,7 @@ class AppDimens {
   static double space_20 = 20.sp;
   static double space_24 = 24.sp;
   static double space_36 = 36.sp;
+  static double space_40 = 40.sp;
   static double space_48 = 48.sp;
 
 
@@ -49,6 +51,7 @@ class AppDimens {
   static double height_90 = 90.h;
   static double height_102 = 102.h;
   static double height_112 = 112.h;
+  static double height_120 = 120.h;
   static double height_122 = 122.h;
   static double height_160 = 160.h;
   static double height_168 = 168.h;
@@ -70,9 +73,15 @@ class AppDimens {
   static double width_6 = 6.w;
   static double width_8 = 8.w;
   static double width_12 = 12.w;
+  static double width_20 = 20.w;
   static double width_16 = 16.w;
   static double width_56 = 56.w;
   static double width_40 = 40.w;
+  static double width_84 = 84.w;
+
+
+
+
 
 
 
