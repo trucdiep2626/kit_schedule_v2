8d048b4fcf300909597139fbad2d67b3dfca23aa
dieptt <trucdiep2626@gmail.com>
2023-03-12 19:47:31 +0700

    add api key

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kit_schedule_v2/common/config/database/hive_config.dart';
 import 'package:kit_schedule_v2/services/local_notification_service.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'common/common_export.dart';
 import 'presentation/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp();
+  await dotenv.load(fileName: ".env");
   LocalNotificationService.initNotificationLocal();
   configLocator();
   final hiveSetUp = getIt<HiveConfig>();
