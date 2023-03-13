26ef9fe743926a6dcc4ea18f100cce81ad3c9bc9
trucdiep2626 <trucdiep2626@gmail.com>
2023-03-13 17:23:48 +0700

    fix update data on login

--- lib/presentation/journey/login/login_controller.dart
+++ lib/presentation/journey/login/login_controller.dart
@@ -4,13 +4,11 @@ import 'package:kit_schedule_v2/common/common_export.dart';
 import 'package:kit_schedule_v2/common/config/network/api_exceptions.dart';
 import 'package:kit_schedule_v2/common/config/network/network_state.dart';
 import 'package:kit_schedule_v2/common/utils/analytics_utils.dart';
-import 'package:kit_schedule_v2/domain/models/score_model.dart';
 import 'package:kit_schedule_v2/domain/models/student_info_model.dart';
 import 'package:kit_schedule_v2/domain/usecases/school_usecase.dart';
 import 'package:kit_schedule_v2/presentation/controllers/analytics_controller.dart';
 import 'package:kit_schedule_v2/presentation/controllers/mixin/export.dart';
 import 'package:kit_schedule_v2/presentation/journey/login/login_success_dialog.dart';
-import 'package:kit_schedule_v2/presentation/journey/score/score_controller.dart';
 import 'package:kit_schedule_v2/presentation/widgets/snack_bar/app_snack_bar.dart';
 
 class LoginController extends GetxController with MixinController {
@@ -60,7 +58,6 @@ class LoginController extends GetxController with MixinController {
           sharePreferencesConstants.setIsLogIn(isLogIn: true);
         }
         getIt<AnalyticsController>().logEvent(AnalyticsEventType.login);
-        Get.offAndToNamed(AppRoutes.main);
         loginSuccessDialog(Get.context!);
       } else {
         showTopSnackBar(
